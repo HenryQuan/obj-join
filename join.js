4c05d1ab24ed33a42f3a0a70a4301bda699b4700
Yiheng (NUC) <development.henryquan@gmail.com>
2019-03-28 22:21:03 +1100

    real world data
    this is for a new feature of wows info...

--- join.js
+++ join.js
@@ -1,36 +1,5 @@
-const a = {
-    hello: {
-        world: 999
-    },
-    number: {
-        one: 2
-    },
-    test: {
-        two: 3
-    },
-    extra: {
-        more: {
-            info: 1
-        }
-    }
-};
-
-const b = {
-    hello: {
-        world: 3
-    },
-    number: {
-        one: 2
-    },
-    test: {
-        two: 1
-    },
-    extra: {
-        more: {
-            info: 1
-        }
-    }
-};
+// Real life testing data from wargaming api
+const ship = JSON.parse('{"status":"ok","meta":{"count":1,"hidden":null},"data":{"2011774448":[{"pvp":{"max_xp":3810,"damage_to_buildings":0,"main_battery":{"max_frags_battle":4,"frags":74,"hits":8145,"shots":21738},"suppressions_count":0,"max_damage_scouting":214831,"art_agro":143713950,"ships_spotted":917,"second_battery":{"max_frags_battle":0,"frags":0,"hits":0,"shots":0},"xp":428873,"survived_battles":229,"dropped_capture_points":1915,"max_damage_dealt_to_buildings":0,"torpedo_agro":41299953,"draws":0,"battles_since_510":374,"planes_killed":58,"battles":374,"max_ships_spotted":9,"team_capture_points":53724,"frags":291,"damage_scouting":18019964,"max_total_agro":1627702,"max_frags_battle":5,"capture_points":19105,"ramming":{"max_frags_battle":1,"frags":1},"torpedoes":{"max_frags_battle":4,"frags":179,"hits":1009,"shots":26375},"aircraft":{"max_frags_battle":0,"frags":0},"survived_wins":163,"max_damage_dealt":211613,"wins":198,"losses":176,"damage_dealt":18371047,"max_planes_killed":3,"max_suppressions_count":0,"team_dropped_capture_points":22023,"battles_since_512":374},"last_battle_time":1552554202,"account_id":2011774448,"distance":22230,"updated_at":1552554881,"battles":384,"ship_id":4282267344,"private":null}]}}');
 
 /**
  * Take in two objs and mode ('a' for sum, 's' for subtract)
@@ -55,5 +24,5 @@ ObjJoin = (a, b, mode) => {
 }
 
 // obj is basically a and this is not really good although it works
-let obj = ObjJoin(a, b, '+');
-console.log(obj);
+let obj = ObjJoin(ship, ship, '-');
+console.log(obj.data['2011774448'][0].pvp);
